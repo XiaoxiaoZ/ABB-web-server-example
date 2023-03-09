38cfa16b8c78445807134df0d2160822976304cb
XiaoxiaoZ <zxxzxxv@gmail.com>
2023-03-09 14:18:55 +0100

    update var, robottarget and wobj

---
+++ abbwebservice.py
@@ -0,0 +1,52 @@
+import requests
+import xml.etree.ElementTree as ET
+
+class ABBWebService:
+    """Class implenmenting ABB Robot Web Services.
+    """
+
+    def __init__(self, base_url="http://localhost/rw/rapid/symbol/data/RAPID", username='Default User', password='robotics'):
+        self.ip = base_url
+        self.robot = "T_ROB1"
+        self.module = "Module1"
+        self.username = username
+        self.password = password
+        self.auth=requests.auth.HTTPDigestAuth(self.username, self.password)
+
+        
+    def update_RAPID_var(self, name, value):
+        url = self.ip+"/"+self.robot+"/"+self.module+"/"+name+"?action=set"
+        data = {"value": value}
+        rp=requests.post(url, auth=self.auth, data=data)
+        #url = ip+"/"+robot+"/"+module+"/"+name
+        #response = requests.get(url,auth=requests.auth.HTTPDigestAuth(username, password))
+        #root = ET.fromstring(response.text)
+        # Find the element with the class "rap-data"
+        #rap_data = root.find(".//*[@class='rap-data']")
+
+        # Find the <span> element with the class "value" inside "rap-data"
+        #response_data = rap_data.find(".//*[@class='value']")
+
+        # Print the result  
+        #while(not math.isclose(float(response_data.text), value)):
+            #print("waiting for sugnal to update")
+        print(rp.text)
+        print("Variable "+name+" is updated to "+str(value)+"!")
+    
+    def set_robtarget(self, var, trans, rot):
+        """Sets the robtarget variable in RAPID.
+        """
+        #_trans, rot = self.get_robtarget_variables(var)
+        self.update_RAPID_var(var, "[[" + ','.join(
+            [str(s) for s in trans]) + "]"+ ",[" +','.join(
+            [str(s) for s in rot]) + "],[-1,0,0,0],[9E+9,9E+9,9E+9,9E+9,9E+9,9E+9]]")
+    
+    def set_wobj(self, var, trans, rot):
+        self.update_RAPID_var(var,"[FALSE,TRUE,\"\","+ "[[" + ','.join(
+            [str(s) for s in trans]) + "]"+ ",[" +','.join(
+            [str(s) for s in rot]) +"]],[[0,0,0],[1,0,0,0]]]")
+    
+serviceA = ABBWebService()
+serviceA.update_RAPID_var("offX",122)
+serviceA.set_robtarget("nocam",[100.0,0.1,0.1],[0.0,0.1,0.2,0.1])
+serviceA.set_wobj("wobj_cam",[200.0,20.323,232.323],[0.1,0.33,0.2,0.2])
--- example.py
+++ example.py
@@ -1,10 +1,44 @@
 import requests
+import xml.etree.ElementTree as ET
+import math
 
-url = "http://localhost/rw/rapid/symbol/data/RAPID/T_ROB1/Module1/offX?action=set"
-username = "Default User"
-password = "robotics"
-data = {"value": "666"}
 
-response = requests.post(url, auth=requests.auth.HTTPDigestAuth(username, password), data=data)
+def update_RAPID_var(name, value):
+    ip = "http://localhost/rw/rapid/symbol/data/RAPID"
+    robot = "T_ROB1"
+    module = "Module1"
+    url = ip+"/"+robot+"/"+module+"/"+name+"?action=set"
+    username = "Default User"
+    password = "robotics"
+    data = {"value": value}
 
-print(response.text)
+    rp=requests.post(url, auth=requests.auth.HTTPDigestAuth(username, password), data=data)
+    #url = ip+"/"+robot+"/"+module+"/"+name
+    #response = requests.get(url,auth=requests.auth.HTTPDigestAuth(username, password))
+    #root = ET.fromstring(response.text)
+    # Find the element with the class "rap-data"
+    #rap_data = root.find(".//*[@class='rap-data']")
+
+    # Find the <span> element with the class "value" inside "rap-data"
+    #response_data = rap_data.find(".//*[@class='value']")
+
+    # Print the result  
+    #while(not math.isclose(float(response_data.text), value)):
+        #print("waiting for sugnal to update")
+    print(rp.text)
+    print("Variable "+name+" is updated to "+str(value)+"!")
+    
+def set_robtarget_translation(var, trans):
+    """Sets the translational data of a robtarget variable in RAPID.
+    """
+    #_trans, rot = self.get_robtarget_variables(var)
+    update_RAPID_var(var, "[[" + ','.join(
+        [str(s) for s in trans]) + "],[0,1,0,0],[-1,0,0,0],[9E+9,9E+9,9E+9,9E+9,9E+9,9E+9]]")
+
+def set_wobj(var, trans):
+
+    update_RAPID_var(var,"[FALSE,TRUE,\"\",[[0.0,65.1388,-4.75403],[0.999928,-0.00335534,-0.00260265,0.011206]],[[0,0,0],[1,0,0,0]]]")
+    
+update_RAPID_var("offX",90)
+set_robtarget_translation("nocam",[0.0,0.1,0.1])
+set_wobj("wobj_cam",[])
